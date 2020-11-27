b2c53d39070619070e24f565a3c734e4dc9e23a0
Matthew Wolff <mm.wolff@chi-squared.org>
2020-11-26 21:30:05 -0600

    achieved convergence

--- Simulator/backwards_euler.py
+++ Simulator/backwards_euler.py
@@ -1,42 +1,48 @@
-from random import sample
-from typing import List
+import logging
+from itertools import count
 
 import numpy as np
 
-NUM_GENES: int = 50
-ACTION_SIZE: int = 10  # number of genes chosen to modify per action
-EPSILON: float = 0.001
-I: np.array = np.identity(NUM_GENES)
-CUTOFF = 500
 
-ODE_COEFFICIENTS: np.array = np.random.rand(NUM_GENES, NUM_GENES)  # a
-
-
-def make_action() -> List[int]:
-    return sample(range(NUM_GENES), ACTION_SIZE)
+def make_symmetric_random():
+    tmp = np.random.rand(NUM_GENES, NUM_GENES)
+    sym = (tmp + tmp.T) / 2
+    sym[np.triu_indices_from(sym)] = -sym[np.triu_indices_from(sym)]
+    return sym
 
 
-def backwards_euler(x_n: np.array, perturbation, delta=0.01):
+def backwards_euler(x_n: np.array, perturbation):
     # in the event of oscillation, a cutoff will implicitly choose a state
     x_n_1 = None
-    for step in range(CUTOFF):
-        print(f"iter {step}: {x_n_1}")
-        coeff = ODE_COEFFICIENTS * delta - I
-        const = -x_n - delta * perturbation
+    for step in count(1):
+        coeff = ODE_COEFFICIENTS * DELTA - I
+        const = -x_n - DELTA * perturbation
         x_n_1 = np.linalg.solve(coeff, const)
 
         # stop if converged
         if np.abs(x_n_1 - x_n).sum() < EPSILON:
-            print("## CONVERGED ##")
-            return x_n_1
+            # CONVERGED
+            break
+        elif step == CUTOFF:
+            logging.warning(f"Backwards Euler did not converge after {CUTOFF} iterations. "
+                            f"Returning anyway.")
+            break
         else:
-            print("total diff:", np.abs(x_n_1 - x_n).sum())
             x_n = x_n_1
 
     return x_n_1
 
 
+NUM_GENES: int = 50
+EPSILON: float = 1E-4
+DELTA = 1E-3
+I: np.array = np.identity(NUM_GENES)
+ODE_COEFFICIENTS: np.array = make_symmetric_random()
+
+# for EPSILON=1E-4, DELTA=1E-3, converged on average around 12.3 (sample size: 50)
+CUTOFF = 16000
+
 if __name__ == "__main__":
     init = np.random.rand(NUM_GENES, 1)  # x
-    adjust = 0 * np.random.rand(NUM_GENES, 1)  # b
-    nextState = backwards_euler(init, perturbation=adjust)
+    adjust = np.random.rand(NUM_GENES, 1)  # b
+    print(backwards_euler(init, adjust))
