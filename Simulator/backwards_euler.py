1276abf45703114d5cb519cc173fb0dbee05d430
Matthew Wolff <mm.wolff@chi-squared.org>
2020-11-27 18:01:40 -0600

    performance testing

--- Simulator/backwards_euler.py
+++ Simulator/backwards_euler.py
@@ -1,8 +1,11 @@
 import logging
+import time
 from itertools import count
 
 import numpy as np
 
+
+# np.random.seed(1996)
 
 def make_symmetric_random():
     tmp = np.random.rand(NUM_GENES, NUM_GENES)
@@ -11,7 +14,7 @@ def make_symmetric_random():
     return sym
 
 
-def backwards_euler(x_n: np.array, perturbation):
+def backwards_euler(x_n: np.array, perturbation, get_steps=False):
     # in the event of oscillation, a cutoff will implicitly choose a state
     x_n_1 = None
     for step in count(1):
@@ -29,8 +32,20 @@ def backwards_euler(x_n: np.array, perturbation):
             break
         else:
             x_n = x_n_1
+    print(f"Converged in {step} steps")
+    if get_steps:
+        return step
 
     return x_n_1
+
+
+def performance():
+    global ODE_COEFFICIENTS
+    ODE_COEFFICIENTS = make_symmetric_random()
+    start = time.time()
+    steps = backwards_euler(np.random.rand(NUM_GENES, 1), np.random.rand(NUM_GENES, 1), get_steps=True)
+    end = time.time()
+    return end - start, steps
 
 
 NUM_GENES: int = 50
@@ -45,4 +60,11 @@ CUTOFF = 16000
 if __name__ == "__main__":
     init = np.random.rand(NUM_GENES, 1)  # x
     adjust = np.random.rand(NUM_GENES, 1)  # b
-    print(backwards_euler(init, adjust))
+    print("x_{n} =", init.round(3).ravel())
+    print("perturb =", adjust.round(3).ravel())
+    print("coefficients =\n", ODE_COEFFICIENTS)
+    print("x_{converged} =", backwards_euler(init, adjust).round(3).ravel())
+
+    times, steps = zip(*[performance() for _ in range(50)])
+    print("average time:", sum(times) / len(times))
+    print("average steps:", sum(steps) / len(steps))
