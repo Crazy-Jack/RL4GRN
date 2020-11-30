fd0b83d447507c91e16d1545345a1567fa2cefd0
Matthew Wolff <mm.wolff@chi-squared.org>
2020-11-30 14:48:33 -0600

    quality of life & standardization changes

--- Simulator/plotting.py
+++ Simulator/plotting.py
@@ -17,16 +17,20 @@ class CoefficientGraph:
         :param edge_threshold: the percentile that edges must be above to be shown
         """
         self.coefficients: np.array = coefficients
-        self._validate_input()
-        self.G = nx.Graph()
-        self.edges = list()
-        self._weights = list()
         self.edge_threshold = edge_threshold
+
+        self._validate_input()
+        self._G = nx.Graph()
+        self._edges = list()
+        self._weights = list()
         self._make_graph()
 
     def _validate_input(self):
         assert isinstance(self.coefficients, np.ndarray)
         assert len(self.coefficients) == len(self.coefficients[0])  # square
+
+        assert isinstance(self.edge_threshold, float)
+        assert 0 <= self.edge_threshold < 1
 
     def _make_graph(self):
         # scale the coefficients to be between 0-1
@@ -36,15 +40,15 @@ class CoefficientGraph:
         nodes = len(self.coefficients)
         for u, v in combinations(range(nodes), 2):
             if normalized[u, v] >= self.edge_threshold:
-                self.G.add_edge(u + 1, v + 1, weight=normalized[u, v])
-                self.edges.append((u + 1, v + 1))
+                self._G.add_edge(u + 1, v + 1, weight=normalized[u, v])
+                self._edges.append((u + 1, v + 1))
                 self._weights.append(normalized[u, v] * WEIGHT_SCALING)
 
     def plot(self):
-        pos = nx.spring_layout(self.G)
-        nx.draw_networkx_nodes(self.G, pos, node_size=700)
-        nx.draw_networkx_edges(self.G, pos, edgelist=self.edges, width=self._weights)
-        nx.draw_networkx_labels(self.G, pos, font_size=16, font_family="sans-serif", font_color="white")
+        pos = nx.spring_layout(self._G)
+        nx.draw_networkx_nodes(self._G, pos, node_size=700)
+        nx.draw_networkx_edges(self._G, pos, edgelist=self._edges, width=self._weights)
+        nx.draw_networkx_labels(self._G, pos, font_size=16, font_family="sans-serif", font_color="white")
         plt.title("Network Coefficients")
         plt.show()
 
