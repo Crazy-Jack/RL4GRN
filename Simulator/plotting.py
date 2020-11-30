48039950534b5b28bc9469bdcacb5e33a81b91bb
Matthew Wolff <mm.wolff@chi-squared.org>
2020-11-30 15:13:03 -0600

    dealt with negative values

--- Simulator/plotting.py
+++ Simulator/plotting.py
@@ -5,7 +5,7 @@ import networkx as nx
 import numpy as np
 
 EPSILON = 1E-12
-WEIGHT_SCALING = 3  # an edge with weight 0.1 is very faint. scale it up
+WEIGHT_SCALING = 3  # improve visibility
 
 
 class CoefficientGraph:
@@ -23,6 +23,7 @@ class CoefficientGraph:
         self._G = nx.Graph()
         self._edges = list()
         self._weights = list()
+        self._colors = list()
         self._make_graph()
 
     def _validate_input(self):
@@ -33,27 +34,31 @@ class CoefficientGraph:
         assert 0 <= self.edge_threshold < 1
 
     def _make_graph(self):
-        # scale the coefficients to be between 0-1
-        small, large = self.coefficients.min(), self.coefficients.max()
-        normalized = (self.coefficients - small) / (large - small + EPSILON)
+        # scale the absolute coefficients to be between 0-1
+        negative = self.coefficients < 0
+        coefficients = np.abs(self.coefficients)
+        small, large = coefficients.min(), coefficients.max()
+        normalized = (coefficients - small) / (large - small + EPSILON)
 
         nodes = len(self.coefficients)
         for u, v in combinations(range(nodes), 2):
-            if normalized[u, v] >= self.edge_threshold:
-                self._G.add_edge(u + 1, v + 1, weight=normalized[u, v])
+            weight = normalized[u, v]
+            if weight >= self.edge_threshold:
+                self._G.add_edge(u + 1, v + 1, weight=weight)
                 self._edges.append((u + 1, v + 1))
-                self._weights.append(normalized[u, v] * WEIGHT_SCALING)
+                self._weights.append(weight * WEIGHT_SCALING)
+                self._colors.append("darkred" if negative[u, v] else "darkgreen")
 
     def plot(self):
         pos = nx.spring_layout(self._G)
-        nx.draw_networkx_nodes(self._G, pos, node_size=700)
-        nx.draw_networkx_edges(self._G, pos, edgelist=self._edges, width=self._weights)
+        nx.draw_networkx_nodes(self._G, pos, node_size=700, node_color="darkgreen")
+        nx.draw_networkx_edges(self._G, pos, edgelist=self._edges, width=self._weights, edge_color=self._colors)
         nx.draw_networkx_labels(self._G, pos, font_size=16, font_family="sans-serif", font_color="white")
         plt.title("Network Coefficients")
         plt.show()
 
 
 if __name__ == "__main__":
-    c = np.random.random((10, 10))
-    g = CoefficientGraph(c)
+    c = np.random.uniform(-1, 2, (10, 10))
+    g = CoefficientGraph(c, edge_threshold=0.2)
     g.plot()
