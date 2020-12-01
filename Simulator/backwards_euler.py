317e8d82a0feda451f11779b5f7ec5c6cd1dcab4
Tianqin Li <jacklitianqin@gmail.com>
2020-11-30 20:57:49 -0500

    seperate backward euler and simulator

--- Simulator/backwards_euler.py
+++ Simulator/backwards_euler.py
@@ -40,11 +40,11 @@ def backwards_euler(x_n, perturbation, CUTOFF, ODE_COEFFICIENTS, DELTA, EPSILON,
     return x_n_1
 
 
-def performance():
+def performance(NUM_GENES, CUTOFF, DELTA, EPSILON):
     # global ODE_COEFFICIENTS
-    ODE_COEFFICIENTS = make_symmetric_random()
+    ODE_COEFFICIENTS = make_symmetric_random(NUM_GENES)
     start = time.time()
-    steps = backwards_euler(np.random.rand(NUM_GENES, 1), np.random.rand(NUM_GENES, 1), get_steps=True)
+    steps = backwards_euler(np.random.rand(NUM_GENES, 1), np.random.rand(NUM_GENES, 1), CUTOFF, ODE_COEFFICIENTS, DELTA, EPSILON, get_steps=True)
     end = time.time()
     return end - start, steps
 
@@ -56,7 +56,7 @@ def test_backward():
     EPSILON: float = 1E-4
     DELTA = 1E-3
     I: np.array = np.identity(NUM_GENES)
-    ODE_COEFFICIENTS: np.array = make_symmetric_random()
+    ODE_COEFFICIENTS: np.array = make_symmetric_random(NUM_GENES)
 
     # for EPSILON=1E-4, DELTA=1E-3, converged on average around 12.3 (sample size: 50)
     CUTOFF = 16000
@@ -66,108 +66,13 @@ def test_backward():
     print("x_{n} =", init.round(3).ravel())
     print("perturb =", adjust.round(3).ravel())
     print("coefficients =\n", ODE_COEFFICIENTS)
-    print("x_{converged} =", backwards_euler(init, adjust).round(3).ravel())
+    print("x_{converged} =", backwards_euler(init, adjust, CUTOFF, ODE_COEFFICIENTS, DELTA, EPSILON).round(3).ravel())
 
-    times, steps = zip(*[performance() for _ in range(50)])
+    times, steps = zip(*[performance(NUM_GENES, CUTOFF, DELTA, EPSILON) for _ in range(50)])
     print("average time:", sum(times) / len(times))
     print("average steps:", sum(steps) / len(steps))
 
 
 
-
-# ==================================
-
-class SimulatorEnv:
-    """class for simulation environment
-    Usage: 
-        env = SimulatorEnv(coefficient, init_state, target_state, time_limit, euler_limit, delta, eps_euler, eps_target, lambda_distance_reward)
-        next_state, reward, done, info, distance_to_target = env(action)
-    See `test_simulator` for concrete example.
-    """
-    def __init__(self, coefficient, init_state, target_state, time_limit, euler_limit, delta, eps_euler, eps_target, lambda_distance_reward):
-        self.num_genes = coefficient.shape[0]
-        self.coefficient = coefficient # np.array [num_genes, num_genes]
-        self.target_state = target_state # np.arrya [num_genes,]
-        self.init_state = init_state # np.arrya [num_genes,]
-        self.time_limit = time_limit # time limit for an episode, one eposode corresponds to certain number of actions
-        self.euler_limit = euler_limit # the maximum delta_t can take for integration when calling backward euler
-        self.delta = delta # delta in integration method 
-        self.eps_euler = eps_euler # epsilon used in backward eular
-        self.eps_target = eps_target # epsilon used for deciding if the current state is close enough to the final state
-        self.lambda_distance_reward = lambda_distance_reward # lambda that controls the weight for the reward 
-
-        # initial state
-        self.reset()
-    
-    def get_reward(self, next_state):
-        """given next state, calculating the reward"""
-
-        # distance to target state
-        distance_to_target = ((self.target_state - next_state) ** 2).sum()
-
-        # eval if the episode ends
-        if distance_to_target < self.eps_target:
-            done = True
-            info = "reach the goal (error {})".format(distance_to_target)
-        elif self.accumulate_step >= self.time_limit:
-            done = True
-            info = "reach the end of episode {}".format(self.accumulate_step)
-        else:
-            done = False
-            info = "keep trying, step {} / {}".format(self.accumulate_step, self.time_limit)
-        
-        # calculate reward
-        reward = - distance_to_target * self.lambda_distance_reward - 1 
-        return reward, done, info, distance_to_target
-
-    def step(self, action):
-        """take an action (perturb), output (next_state, reward, done, info)"""
-        # use backward euler for integrate
-        next_state = backwards_euler(self.state, action, self.euler_limit, self.coefficient, self.delta, self.eps_euler)
-        # calculate next state
-        reward, done, info, distance_to_target = self.get_reward(next_state)
-        # update the state
-        self.state = next_state
-        self.accumulate_step += 1
-        return next_state, reward, done, info, distance_to_target
-    
-    def reset(self):
-        """reset the env"""
-        self.state = self.init_state
-        self.accumulate_step = 0
-        return self.state
-
-
-def test_simulator():
-    # define parameters
-    NUM_GENES = 100
-    time_limit = 200
-    euler_limit = 16000
-    delta = 1e-2
-
-    eps_euler = 1e-4
-    eps_target = 1e-2
-    lambda_distance_reward = 0.1 # reward = - distance_to_target * lambda_distance_reward - 1
-
-    coefficient = make_symmetric_random(NUM_GENES)
-    init_state = np.random.rand(NUM_GENES, 1)
-    target_state = np.random.rand(NUM_GENES, 1)
-    
-    # define env
-    env = SimulatorEnv(coefficient, init_state, target_state, time_limit, euler_limit, delta, eps_euler, eps_target, lambda_distance_reward)
-
-    for i in range(time_limit):
-        # generate random action, replace this with policy network in reinforcement learning
-        action = np.random.rand(NUM_GENES, 1)
-        # put action into environment for evulation
-        start_time = time.time()
-        next_state, reward, done, info, distance_to_target = env.step(action) # env will update its current state after taking every step
-        end_time = time.time()
-        time_delta_step = end_time - start_time
-        print("time: {}; reward: {}; done {}; distance_to_target {}".format(time_delta_step, reward, done, distance_to_target))
-        print(info)
-
-
 if __name__ == "__main__":
-    # test_backward()
-    test_simulator()
+    test_backward()
---
+++ Simulator/simulator.py
@@ -0,0 +1,167 @@
+
+
+
+# ==================================
+
+class SimulatorEnv:
+    """class for simulation environment
+    Usage: 
+        env = SimulatorEnv(coefficient, init_state, target_state, time_limit, euler_limit, delta, eps_euler, eps_target, lambda_distance_reward)
+        next_state, reward, done, info, distance_to_target = env(action)
+    See `test_simulator` for concrete example.
+    """
+    def __init__(self, coefficient, init_state, target_state, original_perturb, \
+                    action_index, max_action, time_limit, euler_limit, delta, eps_euler, eps_target, lambda_distance_reward):
+        self.num_genes = coefficient.shape[0]
+        self.coefficient = coefficient # np.array [num_genes, num_genes]
+        self.original_perturb = original_perturb # original perturb, if certain genes is not actionable through action, it remains the original perturbation term
+        self.target_state = target_state # np.arrya [num_genes,]
+        self.init_state = init_state # np.arrya [num_genes,]
+        self.state_space = len(self.init_state)
+        self.time_limit = time_limit # time limit for an episode, one eposode corresponds to certain number of actions
+        self.euler_limit = euler_limit # the maximum delta_t can take for integration when calling backward euler
+        self.delta = delta # delta in integration method 
+        self.eps_euler = eps_euler # epsilon used in backward eular
+        self.eps_target = eps_target # epsilon used for deciding if the current state is close enough to the final state
+        self.lambda_distance_reward = lambda_distance_reward # lambda that controls the weight for the reward 
+
+        # initial state
+        self.reset() # reassign the state into self.init_state
+
+        # define action space
+        self.action_index = action_index # a list, define action space index (e.g [1,3,4,8]), indicating which genes in [num_genes,] vector is actionable
+        self.action_space = len(self.action_index)
+        self.max_action = max_action # scalar, 
+
+        # final check
+        self.self_assert()
+
+
+    def self_assert(self):
+        """check if the init is reasonable"""
+        assert self.action_space < self.state_space # only selected not all of the genes can be modified
+        assert self.state_space == len(self.target_state) # target space is matched with the init state space
+
+    
+    def get_reward(self, next_state):
+        """given next state, calculating the reward"""
+
+        # distance to target state
+        distance_to_target = ((self.target_state - next_state) ** 2).sum()
+
+        # eval if the episode ends
+        if distance_to_target < self.eps_target:
+            done = True
+            info = "reach the goal (error {})".format(distance_to_target)
+        elif self.accumulate_step >= self.time_limit:
+            done = True
+            info = "reach the end of episode. ".format(self.accumulate_step)
+        else:
+            done = False
+            info = "keep trying, step {} / {}".format(self.accumulate_step, self.time_limit)
+        
+        # calculate reward
+        reward = - distance_to_target * self.lambda_distance_reward - 1 
+        return reward, done, info, distance_to_target
+
+    def step(self, action):
+        """take an action (perturb), output (next_state, reward, done, info)
+        param: 
+            - action: # [self.action_space, ]
+        """
+        # construct the resulted perturb
+        assert len(action) == self.action_space
+        perturb = self.original_perturb
+        perturb[self.action_index] = action
+        # use backward euler for integrate
+        next_state = backwards_euler(self.state, perturb, self.euler_limit, self.coefficient, self.delta, self.eps_euler)
+        # calculate next state
+        reward, done, info, distance_to_target = self.get_reward(next_state)
+        # update the state
+        self.state = next_state
+        self.accumulate_step += 1
+        return next_state, reward, done, info, distance_to_target
+    
+    def reset(self):
+        """reset the env"""
+        self.state = self.init_state
+        self.accumulate_step = 0
+        return self.state
+    
+
+# store the parameters for specific environment
+def make(env_name, seed_network, seed_init):
+    """Automatically make environment"""
+    if env_name == 'random_generate_example':
+        NUM_GENES = 100
+        time_limit = 20
+        euler_limit = 16000
+        action_percent = 0.3
+        delta = 1e-2
+        eps_euler = 1e-4
+        eps_target = 1e-2
+        lambda_distance_reward = 0.1 # reward = - distance_to_target * lambda_distance_reward - 1
+
+        # init the network structure and the actionable genes
+        np.random.seed(seed_network)
+        coefficient = make_symmetric_random(NUM_GENES) # random
+        action_space = int(NUM_GENES * action_percent)
+        action_index = np.random.randint(NUM_GENES, size=(action_space,)) # random
+
+        # init the intial state, the target state
+        np.random.seed(seed_init)
+        init_state = np.random.rand(NUM_GENES, 1) # random 
+        target_state = np.random.rand(NUM_GENES, 1) # random
+        original_perturb = np.random.rand(NUM_GENES, 1) # random
+
+        print(action_index.shape)
+        # define env
+        env = SimulatorEnv(coefficient, init_state, target_state, \
+                    original_perturb, action_index, time_limit, euler_limit, \
+                    delta, eps_euler, eps_target, lambda_distance_reward)
+        return env
+
+    elif env_name == 'random_generate_tune':
+        NUM_GENES = 100
+        time_limit = 200
+        euler_limit = 16000
+        action_percent = 0.3
+        delta = 1e-2
+        eps_euler = 1e-4
+        eps_target = 1e-2
+        lambda_distance_reward = 0.1 # reward = - distance_to_target * lambda_distance_reward - 1
+        seed = 0
+        np.random.seed(seed)
+        coefficient = make_symmetric_random(NUM_GENES) # random
+        init_state = np.random.rand(NUM_GENES, 1) # random 
+        target_state = np.random.rand(NUM_GENES, 1) # random
+        original_perturb = np.random.rand(NUM_GENES, 1) # random
+        action_space = int(NUM_GENES * action_percent)
+        action_index = np.random.randint(NUM_GENES, size=(action_space,))
+        # define env
+        env = SimulatorEnv(coefficient, init_state, target_state, original_perturb, action_index, time_limit, euler_limit, delta, eps_euler, eps_target, lambda_distance_reward)
+        return env
+    else:
+        raise NotImplementedError("Env {} not implemented. ".format(env_name))
+
+
+def test_simulator():
+    # define parameters
+    env = make('random_generate_example', seed_network=0, seed_init=1)
+    train_steps = 200
+    episode = 0
+    for i in range(train_steps):
+        # generate random action, replace this with policy network in reinforcement learning
+        action = np.random.rand(env.action_space, 1)
+        # put action into environment for evulation
+        start_time = time.time()
+        next_state, reward, done, info, distance_to_target = env.step(action) # env will update its current state after taking every step
+        end_time = time.time()
+        time_delta_step = end_time - start_time
+        print("time: {}; reward: {}; done {}; distance_to_target {}".format(time_delta_step, reward, done, distance_to_target))
+        print(info)
+
+        if done:
+            print("Episode {} End. ----------\n".format(episode))
+            env.reset()
+            episode += 1
